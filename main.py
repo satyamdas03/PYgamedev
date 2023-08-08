12e014b7b0a6d9f757ac2cad35f52e3f2cb1649a
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2023-08-08 21:10:51 +0530

    t

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-# new proj
-# to code this in a week
-# idea : use tkinter, gpt
-# use AI maybe to create new scenarios
-# use private new text
-# start coding 8th aug
-# 25% per day
-# after office hours
+import pygame
+
+screen_size = [360,600]
+screen = pygame.display.set_mode(screen_size)
+#  ongoing
